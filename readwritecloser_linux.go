7701863193564b302c4e298d1d57e207d68d4554
Chiel de Roest <chielderoest@gmail.com>
2020-12-15 18:05:45 -0800

    set socket option to receive error frames

--- readwritecloser_linux.go
+++ readwritecloser_linux.go
@@ -8,8 +8,22 @@ import (
 	"syscall"
 )
 
+const (
+	_SOL_CAN_RAW        = 101
+	_CAN_RAW_ERR_FILTER = 2
+)
+
 func NewReadWriteCloserForInterface(i *net.Interface) (ReadWriteCloser, error) {
-	s, _ := syscall.Socket(syscall.AF_CAN, syscall.SOCK_RAW, unix.CAN_RAW)
+	s, err := syscall.Socket(syscall.AF_CAN, syscall.SOCK_RAW, unix.CAN_RAW)
+	if err != nil {
+		return nil, err
+	}
+	allowAllIDsMask := [4]byte{0xff, 0xff, 0xff, 0xff}
+	err = syscall.SetsockoptInet4Addr(s, _SOL_CAN_RAW, _CAN_RAW_ERR_FILTER, allowAllIDsMask)
+	if err != nil {
+		return nil, err
+	}
+
 	addr := &unix.SockaddrCAN{Ifindex: i.Index}
 	if err := unix.Bind(s, addr); err != nil {
 		return nil, err
